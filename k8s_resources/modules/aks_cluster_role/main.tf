4096a41847722f18ce24dbc09d23706adfb4dbde
krishnajugal <44352683+krishnajugal@users.noreply.github.com>
2021-02-15 13:34:40 +0530

    Add files via upload

---
+++ k8s_resources/modules/aks_cluster_role/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.0.2"
+    }
+  }
+}
+
+provider "kubernetes" {
+  features{}
+}
+
+resource "kubernetes_cluster_role" "example" {
+  metadata {
+    name = "terraform-example"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["namespaces", "pods"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
